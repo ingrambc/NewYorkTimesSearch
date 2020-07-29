ae7ef1a97564996cd388afc9381f7380913b4e0b
Tyler Mathews <mrmathews08@gmail.com>
2020-07-28 21:18:18 -0600

    var

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ var startYear = 0;
 var endYear = 0;
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ searchTerm + "&api-key=pUEZfbiM5DRsQV7ZdaVpCdAVjVYAgWJ2";
 
+
+
+function runQuery();
+
+
+
+
   $.ajax({  
     url: queryURL,
     method: "GET"
